b2eff76cf571d2ebf27db9da42f3981a15b38d08
alaurin10 <50841025+alaurin10@users.noreply.github.com>
2022-04-13 17:21:23 -0700

    Updated PC code
    Code now properly decodes UART communication and plots the two step response graphs.

--- Lab Code/Lab 0/PCCode.py
+++ Lab Code/Lab 0/PCCode.py
@@ -1,29 +1,71 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import serial
+import numpy as np
 import os
 
 
-UART_PORT = 'COM10'
-
-time_array = []
-voltage_array = []
+UART_PORT = 'COM10'     # Serial port used for UART to PC
+SAMPLES = 2000          # Number of ADC samples
 
 
+# Function that converts ADC bits to a voltage value
+def convert_to_voltage(adc_value):
+    VLSB = 3.3/4096             # Voltage of Least Signifhicant Bit
+    voltage = VLSB * adc_value  # VLSB * Number of bits
+    return voltage
+
+
+# Function to convert ms to s
+def convert_to_seconds(time_value):
+    return time_value / 1000
+
+
+# Function to convert linear voltage values to log-scale voltage values
+def convert_to_log(voltage_array):
+    log_array = []
+    max_val = max(voltage_array)
+    for value in voltage_array:
+        log_array.append(np.log((max_val - value)/max_val))
+    return log_array
+
+
+# Main function
 def main():
-    os.system("cls")
-    while True:
-        with serial.Serial(UART_PORT, 115200) as ser:
-            if not ser.isOpen():
-                ser.open()
-            line = ser.readline()
-            line = str(line, 'utf-8')
-            line = line.strip('\r\n')
-            line = line.split(',')
-            result = list(map(int, line))            
-            
-            print(result)
+    os.system("cls")    # Clear console
+    raw_data = []       # Define lists
+    time_vals = []
+    voltage_vals = []
+    
+    
+    # Open serial port and collect total number of ADC samples
+    with serial.Serial(UART_PORT, 115200) as ser:
+        for i in range(SAMPLES):
+                    
+            line = ser.readline()       # Read data until line termination: '\n'
+            raw_data.append(line)       # Append data to list
 
 
+    # Convert and separate raw data into time and ADC values
+    for line in raw_data:
+        time, adc = map(int, line.decode().strip('\r\n').split(','))    # Decode data
+        time_vals.append(convert_to_seconds(time))      # Convert and append time values in seconds to list 
+        voltage_vals.append(convert_to_voltage(adc))    # Convert and append adc values in volts to list
+        
+
+    # Plot 1: Linear step response
+    plt.figure(1)
+    plt.plot(time_vals, voltage_vals)
+    plt.xticks(np.arange(min(time_vals), max(time_vals)+2, 0.25))   
+    plt.xlim(0, 2)
+    plt.grid()
+    
+    # Plot 2: log-scale step response
+    log_vals = convert_to_log(voltage_vals)     # Convert linear voltage into log-scale voltage
+    plt.figure(2)
+    plt.plot(time_vals, log_vals)
+    plt.grid()
+    plt.show()
+        
 
 
 if __name__ == '__main__':
