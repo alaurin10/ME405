fa080eaae9bc6df13e3c11c79db7ac548f29badc
alaurin10 <50841025+alaurin10@users.noreply.github.com>
2022-04-13 21:34:30 -0700

    Final Version of PC code

--- Lab Code/Lab 0/PCCode.py
+++ Lab Code/Lab 0/PCCode.py
@@ -17,26 +17,27 @@ def convert_to_voltage(adc_value):
 
 # Function to convert ms to s
 def convert_to_seconds(time_value):
-    return time_value / 1000
+    return time_value / 1000    # Millisecond to seconds
 
 
 # Function to convert linear voltage values to log-scale voltage values
 def convert_to_log(voltage_array):
     log_array = []
-    max_val = max(voltage_array)
-    for value in voltage_array:
-        log_array.append(np.log((max_val - value)/max_val))
+    max_val = max(voltage_array)    # FInd max value of array as Vin
+    for value in voltage_array:        
+        log_array.append(np.log((max_val - value)/max_val))     # Log value equation
     return log_array
 
 
 # Main function
 def main():
     os.system("cls")    # Clear console
+    np.seterr(divide = 'ignore') 
     raw_data = []       # Define lists
     time_vals = []
     voltage_vals = []
     
-    
+
     # Open serial port and collect total number of ADC samples
     with serial.Serial(UART_PORT, 115200) as ser:
         for i in range(SAMPLES):
@@ -57,16 +58,29 @@ def main():
     plt.plot(time_vals, voltage_vals)
     plt.xticks(np.arange(min(time_vals), max(time_vals)+2, 0.25))   
     plt.xlim(0, 2)
+    plt.title('Step Response of RC Circuit', fontsize=15)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Voltage [V]")
     plt.grid()
     
-    # Plot 2: log-scale step response
+    # Calculate log-scale values
     log_vals = convert_to_log(voltage_vals)     # Convert linear voltage into log-scale voltage
+
+    # Find slope of line
+    slope, intercept = np.polyfit(log_vals[:int(SAMPLES/2)], time_vals[:int(SAMPLES/2)], 1)
+    
+
+    # Plot 2: log-scale step response
     plt.figure(2)
     plt.plot(time_vals, log_vals)
+    plt.annotate('Slope = '+str(round(slope, 4)), xy=(0.75,-1.5), fontsize=15)
+    plt.title('Log Scale Step Response of RC Circuit', fontsize=15)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Log Values [-]")
     plt.grid()
     plt.show()
-        
+    
 
-
+# Run main function
 if __name__ == '__main__':
     main()
